03810c818edf9bb54cf06b1bfc1ec0215476f5a0
Hiroki Sato <m@skahack.com>
2012-12-23 20:07:19 +0900

    Updated oh-my-zsh

--- zsh/tools/install.sh
+++ zsh/tools/install.sh
@@ -5,20 +5,16 @@ then
 fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-which git > /dev/null
-if [[ $? -eq 0 ]]; then
-  /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-else
+hash git >/dev/null && /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh || {
   echo "git not installed"
   exit
-fi
+}
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
 then
-  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32]Backing up to ~/.zshrc.pre-oh-my-zsh\033[0m";
-  cp ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
-  rm ~/.zshrc;
+  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-oh-my-zsh\033[0m";
+  mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
 fi
 
 echo "\033[0;34mUsing the Oh My Zsh template file and adding it to ~/.zshrc\033[0m"
